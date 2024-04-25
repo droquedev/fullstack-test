c57a8431e418cc095e5b35573b125a8ff6261841
droquedev <davidroque21@hotmail.com>
2024-04-25 16:23:14 -0400

    chore: fix problems with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   },
 };
